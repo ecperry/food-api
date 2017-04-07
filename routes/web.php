76283a2da8e48fce702984c23f2158e5cf9d9fae
ecperry <ecperry17@gmail.com>
2017-04-07 11:31:45 -0400

    Deleted unnecessary spaces.

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,7 @@
 use Illuminate\Http\Request;
 
 //customers table collection routes
-Route::get('/customers', 'CustomerController@index' );
+Route::get('/customers', 'CustomerController@index');
 Route::post('/customers', 'CustomerController@create');
 
 //customer singular routes
@@ -32,7 +32,7 @@ Route::put('/orders/{id}', 'OrdersController@update');
 Route::delete('/orders/{id}', 'OrdersController@destroy');
 
 //Recipe collection routes
-Route::get('/recipes', 'RecipeController@index' );
+Route::get('/recipes', 'RecipeController@index');
 Route::post('/recipes', 'RecipeController@create');
 
 //Recipe singular routes
